e4fc5576c473becae3d08a346ed33e545ff5d0f3
Muhammad Ali <info@muhammad-ali.uz>
2021-12-29 15:52:24 +0500

    descending

--- logic18.py
+++ logic18.py
@@ -5,6 +5,6 @@ def main(a):
     Returns:
         True if all digits of a are in descending order, False otherwise
     """
-    return a//10000>a//1000%10 and a//1000%10>a//100%10 and a//100%10>a//10%10 and a//10%10>a%10
+    return a//10000<a//1000%10 and a//1000%10<a//100%10 and a//100%10<a//10%10 and a//10%10<a%10
 
-print(main(54321))
+print(main(12345))
